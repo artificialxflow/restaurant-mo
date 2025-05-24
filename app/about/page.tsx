36089d11e79f4bbb42cb7969e6f7ebc9ffc8d062
zedparman <ze.dparman@gmail.com>
2025-05-23 21:57:22 -0700

    Create page.tsx

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <div className="container py-5">
+      <h1 className="mb-3">About</h1>
+      <p>This is the About page. You can add information about your project or company here.</p>
+    </div>
+  );
+} 
