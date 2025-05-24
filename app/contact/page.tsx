cd2b6593812a57c6c50988b2740e19244f1e1256
zedparman <ze.dparman@gmail.com>
2025-05-23 21:57:13 -0700

    Create page.tsx

---
+++ app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function ContactPage() {
+  return (
+    <div className="container py-5">
+      <h1 className="mb-3">Contact</h1>
+      <p>This is the Contact page. You can add your contact information or a contact form here.</p>
+    </div>
+  );
+} 
